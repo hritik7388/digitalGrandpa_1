f624301ce2323814556b886bab8c26b1cd6c04ca
hritik7388 <choreohritik52@gmail.com>
2026-07-29 11:35:32 +0530

    refactor: remove createUser method and validation schema from UserRepository and validator

--- src/modules/userServices/repository.ts
+++ src/modules/userServices/repository.ts
@@ -1,34 +1,9 @@
 // src/modules/userServices/repository.ts
 
 import { UserType } from "@prisma/client";
-import { CreateUserInput } from "./validator";
+// import { CreateUserInput } from "./validator";
 import prisma from "../../config/prisma";
 
 export class UserRepository {
-  async createUser(data: CreateUserInput) {
-    return prisma.$transaction(async (tx) => {
-      const credential = await tx.userCredentials.create({
-        data: {
-          email: data.email,
-          passwordHash: data.passwordHash!,
-        },
-      });
-
-      const profile = await tx.userProfile.create({
-        data: {
-          cred_id: credential.cred_id,
-          firstName: data.firstName,
-          lastName: data.lastName,
-          mobileNumber: data.mobileNumber,
-          countryCode: data.countryCode,
-          user_type: UserType[data.user_type as keyof typeof UserType],
-          userName: "",
-          gender: "",
-          bio: "",
-        },
-      });
-
-      return { credential, profile };
-    });
-  }
+  
 }
