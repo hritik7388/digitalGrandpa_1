59dbcb99cbf631bcc235007f4818bcbae0092602
hritik7388 <choreohritik52@gmail.com>
2026-08-27 16:39:16 +0530

    refactor: streamline getOrderProducts method formatting in OrderRepository

--- src/modules/orderServices/repository.ts
+++ src/modules/orderServices/repository.ts
@@ -3,36 +3,33 @@ import prisma from "../../config/prisma";
 
 // src/modules/orderServices/repository.ts
 export class OrderRepository {
-getOrderProducts = async (orderNumber: string) => { 
+  getOrderProducts = async (orderNumber: string) => {
+    const order = await prisma.order.findUnique({
+      where: {
+        orderNumber: orderNumber,
+      },
+      select: {
+        order_id: true,
+        orderNumber: true,
 
-  const order = await prisma.order.findUnique({
-    where: {
-      orderNumber: orderNumber,
-    },
-    select: {
-      order_id: true,
-      orderNumber: true,
-
-      orderItems: {
-        select: {
-          item_id: true,
-          product_id: true,
-          variant_id: true,
-          title: true,
-          variantTitle: true,
-          image: true,
-          quantity: true,
-          currentQuantity: true,
-          fulfillableQuantity: true,
-          price: true,
-          sku: true,
+        orderItems: {
+          select: {
+            item_id: true,
+            product_id: true,
+            variant_id: true,
+            title: true,
+            variantTitle: true,
+            image: true,
+            quantity: true,
+            currentQuantity: true,
+            fulfillableQuantity: true,
+            price: true,
+            sku: true,
+          },
         },
       },
-    },
-  }); 
+    });
 
-  
-
-  return order;
-};
+    return order;
+  };
 }
